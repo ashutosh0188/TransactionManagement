873cd841677cd7ea26eb1f9a9f41411d95a85154
ashutosh0188 <ashutosh0188@gmail.com>
2019-08-18 19:36:00 +0530

    Added basic springboot maven project structure

---
+++ src/main/java/com/example/springboot/TransactionsManagement/TransactionsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.TransactionsManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsmanagementApplication.class, args);
+	}
+
+}
